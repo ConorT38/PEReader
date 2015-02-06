736b69d238073017d6653c43f0845e087e611243
stacksmash3r <0x4000@mail.ru>
2015-02-06 23:03:02 +0000

    Menu implemented
    Basic menu implemented, just need to add more options and format the
    output so it is nicer to look at.

--- PEReader.c
+++ PEReader.c
@@ -32,7 +32,8 @@ int main()
 		return MALLOC_ERROR;
 	}
 	printf("Enter path to file: ");
-	scanf("%s",fileName); 
+	scanf("%s",fileName);
+	fflush(stdin); 
 	fileHandle = CreateFile(fileName,GENERIC_READ,FILE_SHARE_READ,NULL,OPEN_EXISTING,0,NULL); //Get access to the requsted file and return a pointer to fileHandle
 	if(fileHandle == INVALID_HANDLE_VALUE)
 	{
@@ -64,38 +65,105 @@ int main()
 
 void menu(IMAGE_DOS_HEADER * DOSheader,IMAGE_NT_HEADERS * PEHeader)
 {
+	char main_menu_choice = '\0';
+	char menu_sub_choice = '\0';
+	while(main_menu_choice != '5')
+	{
+		printf("1. DOS header options \n");
+		printf("2. PE header options \n");
+		printf("5. Quit \n");
+		scanf("%c",&main_menu_choice);
+		fflush(stdin);
 
+		if(main_menu_choice == '5')
+		{
+			printf("Goodbye! \n");
+			exit(0);
+		}
+
+		while(menu_sub_choice != '5')
+		{
+			if(main_menu_choice == '1')
+			{
+				printf("DOS header options \n");
+				printf("1. Signature \n");
+				printf("2. Size \n");
+				printf("3. Relocations \n");
+				printf("4. RVA to PE header \n");
+				printf("5. Back \n");
+				scanf("%c",&menu_sub_choice);
+				fflush(stdin);
+				walk_dos_header(DOSheader,menu_sub_choice);
+			}
+			else if(main_menu_choice == '2')
+			{
+				printf("PE header options \n");
+				printf("1. Signature \n");
+				printf("2. Number of sections \n");
+				printf("3. Optional header size \n");
+				printf("4. Entry point \n");
+				printf("5. Back \n");
+				scanf("%c",&menu_sub_choice);
+				fflush(stdin);
+				walk_pe_header(PEHeader,menu_sub_choice);
+			}
+			
+		}
+		main_menu_choice = '\0';
+		menu_sub_choice = '\0';
+	}
+	
 }
 
 void walk_pe_header(IMAGE_NT_HEADERS * PEHeader,int choice)
 {
-	if(choice == 1)
+	if(choice == '1')
 	{
 		printf("Signature: %.8x \n",PEHeader -> Signature);
+	}
+	else if(choice == '2')
+	{
+		printf("Number of sections: %.8x \n",PEHeader -> FileHeader.NumberOfSections);
+	}
+	else if(choice == '3')
+	{
+		printf("Size of optional header: %.8x \n",PEHeader -> FileHeader.SizeOfOptionalHeader);
+	}
+	else if(choice == '4')
+	{
+		printf("RVA to entry point: %.8x \n",PEHeader -> OptionalHeader.AddressOfEntryPoint);
+	}
+	else if(choice == '5')
+	{
+		printf("Returning to main menu \n");
+	}
+	else
+	{
+		printf("Not an option! \n");
 	}
 }
 
 void walk_dos_header(IMAGE_DOS_HEADER * DOSheader, int choice)
 {
-	if(choice == 1)
+	if(choice == '1')
 	{
 		printf("Signature: %.8x \n",DOSheader -> e_magic);
 	}
-	else if(choice == 2)
+	else if(choice == '2')
 	{
 		printf("Size: %.8x \n",DOSheader -> e_cparhdr);
 	}
-	else if(choice == 3)
+	else if(choice == '3')
 	{
 		printf("Relocations: %.8x \n",DOSheader -> e_crlc);
 	}
-	else if(choice == 4)
+	else if(choice == '4')
 	{
 		printf("RVA to PE Header: %.8x \n",DOSheader -> e_lfanew);
 	}
-	else if(choice == 5)
+	else if(choice == '5')
 	{
-		return;
+		printf("Returning to main menu \n");
 	}
 	else
 	{
@@ -125,52 +193,4 @@ void * getdosheaderHandle(void * base)
 	}
 	
 	return DOSheader;
-
-	/*
-	int i = 0;
-
-	IMAGE_DOS_HEADER * DOSheader = base; //Mostly used to check the MZ signature is present and grab the offset to the start of the PE file
-	IMAGE_NT_HEADERS * PEHeader; //Pointer to the PE header, like DOSHeader but contains more valuable information about the executable file
-	IMAGE_SECTION_HEADER * sectionHeader; 
-
-	if(DOSheader -> e_magic != IMAGE_DOS_SIGNATURE) //Check if the DOS header contains a valid MZ signature
-	{
-		printf("Not a valid PE file! \n");
-	}
-	printf("DOS header signature %.8x \n",DOSheader -> e_magic);
-	printf("Header size: %.8x  \n",DOSheader -> e_cparhdr);
-	printf("Relocations: %.8x \n",DOSheader -> e_crlc);
-	printf("Pointer to PE Header: %.8x \n",DOSheader -> e_lfanew); //RVA to PE Header
-
-	PEHeader = (IMAGE_NT_HEADERS * )((BYTE *)base + DOSheader -> e_lfanew); //Calcuate the absolute address of the PE Header, BASE + RVA 
-
-	if(PEHeader -> Signature != IMAGE_NT_SIGNATURE) //Similar to the DOS Header check, check if the PE Header signature PE\0\0 is present
-	{
-		printf("Invalid PE File! \n");
-	}
-
-	//Output explains the following
-	printf("Number of sections: %.8x \n",PEHeader -> FileHeader.NumberOfSections);
-	printf("Size of optional header: %.8x \n",PEHeader -> FileHeader.SizeOfOptionalHeader);
-	printf("Magic: %.8x \n", PEHeader -> OptionalHeader.Magic);
-	printf("Size of .code: %.8x \n",PEHeader -> OptionalHeader.SizeOfCode); 
-	printf("Size of .data: %.8x \n",PEHeader -> OptionalHeader.SizeOfUninitializedData); //shows the size of the data file
-	printf("Size of .bss: %.8x \n",PEHeader -> OptionalHeader.SizeOfInitializedData);
-	printf("RVA to entry point: %.8x \n",PEHeader -> OptionalHeader.AddressOfEntryPoint);
-	printf("RVA to start of .code: %.8x \n", PEHeader -> OptionalHeader.BaseOfCode);
-	printf("RVA to start of .data: %.8x \n", PEHeader -> OptionalHeader.BaseOfData);
-	printf("Preferred load address: %.8x \n", PEHeader -> OptionalHeader.ImageBase);
-	printf("Section alignment in memory: %.8x \n", PEHeader -> OptionalHeader.SectionAlignment);
-	printf("File Alignment: %.8x \n", PEHeader -> OptionalHeader.FileAlignment);
-	printf("Offset from start of file to first section: %.8x \n", PEHeader -> OptionalHeader.SizeOfHeaders);
-	printf("Number of RVA's: %.8x \n", IMAGE_NUMBEROF_DIRECTORY_ENTRIES); //More reliable
-
-	sectionHeader = PEHeader -> FileHeader.SizeOfOptionalHeader + 1; //Sections are right after the end of the optionalheader, so the size of the optional header points to the end
-
-	for(i; i < 10; i++) //There is normally 10-14 sections there could be more though, will fix this later so it isn't hardcoded, testing for now
-	{
-		printf("RVA %.8x \n",PEHeader -> OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT + i]); //add 1 to the define which starts 0 so third iteration would be 0 + 3, could just use i but this way is more clear
-	}
-	*/
-
 }
